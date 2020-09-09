e502a18a8510f42e268b0a718f547b4a5df82d25
Beau Hale <beauhale@Beaus-Air.attlocal.net>
2020-09-08 21:45:49 -0700

    first commit

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYg5BX06cDl2waqFIF4QbK3iIO-EFy1GE",
+    authDomain: "challenge-bf9c3.firebaseapp.com",
+    databaseURL: "https://challenge-bf9c3.firebaseio.com",
+    projectId: "challenge-bf9c3",
+    storageBucket: "challenge-bf9c3.appspot.com",
+    messagingSenderId: "1058297127161",
+    appId: "1:1058297127161:web:cabd5d3e9ab1b47a96c228",
+    measurementId: "G-YXFWE78M5D"
+};
